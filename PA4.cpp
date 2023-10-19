b4f38b183692c07d7ffb058843560714c54bc863
Jose <37120821+zerta92@users.noreply.github.com>
2023-10-19 13:54:39 +0100

    readme

--- PA4.cpp
+++ PA4.cpp
@@ -8,105 +8,90 @@
 
 using namespace std;
 
+int main()
+{
 
-int main(){
-	
 	Model model = Model();
 	GameCommands gc;
 	model.show_status();
 	View view;
 	model.display(view);
-	////while (true){
 
-	
-		//char command;
-		//cout << "Enter Command: ";
-		//cin >> command;
+	while (true)
+	{
+		char command;
+		cout << "Enter Command: ";
+		cin >> command;
 
-	//if 2 fish go to eat second one enters infinite loop when it grows
-	//tuna always mating
+		try
+		{
 
-		//
+			if (command != 'a' && command != 'e' && command != 'f' && command != 'g' && command != 'n' && command != 'r' && command != 'q' && command != 's' && command != 'z')
+				throw InvalidInput("Not a valid command");
 
-		while (true) {
-			char command;
-			cout << "Enter Command: ";
-			cin >> command;
+			switch (command)
+			{
+			case 's':
+				gc.do_swim_command(model);
+				model.display(view);
+				break;
 
-			try {
-				
-				if (command != 'a' && command != 'e' && command != 'f' && command != 'g' && command != 'n' && command != 'r' && command != 'q' && command != 's' && command != 'z')
-					throw InvalidInput("Not a valid command");
-					
+			case 'n':
+				gc.handle_new_command(model);
+				model.display(view);
+				break;
 
-				switch (command){
-				case  's':
-					gc.do_swim_command(model);
-					model.display(view);
-					break;
+			case 'a':
+				gc.do_attack(model);
+				model.display(view);
+				break;
 
+			case 'e':
+				gc.do_eat_command(model);
+				model.display(view);
+				break;
 
-			
+			case 'f':
+				gc.do_float_command(model);
+				model.display(view);
+				break;
 
-				case  'n':
-					gc.handle_new_command(model);
-					model.display(view);
-					break;
+			case 'z':
+				gc.do_zoom_command(model);
+				model.display(view);
+				break;
 
-				case  'a':
-					gc.do_attack(model);
-					model.display(view);
-					break;
+			case 'g':
+				cout << "Advancing one tick" << endl;
+				gc.do_go_command(model);
+				model.show_status();
+				model.display(view);
 
+				break;
 
-				case  'e':
-					gc.do_eat_command(model);
-					model.display(view);
-					break;
+			case 'r':
+				cout << "Advancing next event" << endl;
+				gc.do_run_command(model);
+				model.show_status();
+				model.display(view);
 
-				case  'f':
-					gc.do_float_command(model);
-					model.display(view);
-					break;
+				break;
 
-				case  'z':
-					gc.do_zoom_command(model);
-					model.display(view);
-					break;
-
-				case  'g':
-					cout << "Advancing one tick" << endl;
-					gc.do_go_command(model);
-					model.show_status();
-					model.display(view);
-
-					break;
-
-				case  'r':
-					cout << "Advancing next event" << endl;
-					gc.do_run_command(model);
-					model.show_status();
-					model.display(view);
-
-					break;
-
-
-				case  'q':
-					cout << "Terminating Program" << endl;
-					return 0;
-					//break;
-				}
+			case 'q':
+				cout << "Terminating Program" << endl;
+				return 0;
+				// break;
 			}
-
-			catch (InvalidInput& except) {
-				// actions to be taken if the input is wrong
-				cout << "Invalid input - " << except.msg_ptr << endl;
-			}
-			cin.clear();
-			cin.ignore();
 		}
-		
+
+		catch (InvalidInput &except)
+		{
+			// actions to be taken if the input is wrong
+			cout << "Invalid input - " << except.msg_ptr << endl;
+		}
+		cin.clear();
+		cin.ignore();
+	}
 
 	return 0;
-
 }
---
+++ README.md
@@ -0,0 +1,43 @@
+# Fish Game
+
+A “Warcraft” style game in which you can control a marine ecosystem based on keyboard commands
+
+## Build Instructions
+
+### Prerequisites
+
+Make sure you have the following tools installed on your system:
+
+- C++ Compiler (e.g., GCC, Clang)
+- Make (optional, if you use a Makefile)
+- Visual Studio Code (optional, for editing and running tasks)
+
+### Building the Project
+
+1. **Open a Terminal:**
+   - If you're using Visual Studio Code, open the integrated terminal (View > Terminal or `Ctrl` + `backtick`).
+
+2. **Navigate to the Project Directory:**
+   ```bash
+   cd /path/to/project
+
+   If you have a Makefile:
+    make
+   If you are using a custom build command
+    g++ -g -std=c++11 PA4.cpp GameCommands.cpp CartPoint.cpp CartVector.cpp Cave.cpp CoralReef.cpp Fish.cpp GameObject.cpp Model.cpp Shark.cpp Sharknado.cpp Tuna.cpp View.cpp -o PA4
+    ```
+
+   ### Running the Project
+   1. **Open a Terminal:**
+   - If you're using Visual Studio Code, open the integrated terminal (View > Terminal or `Ctrl` + `backtick`).
+
+   2. **Navigate to the Executable Directory:**
+   ```bash
+   cd /path/to/project
+   ```
+    3. **Navigate to the Executable Directory:**
+   ```bash
+    ./PA4
+   ```
+
+  
